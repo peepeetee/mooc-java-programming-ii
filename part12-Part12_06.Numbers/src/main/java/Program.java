cc6a0d0871283f0b99f624d56872eb8b942a0be2
peepeetee <peepeetee@protonmail.com>
2025-06-06 21:21:27 +0800

    12_07

--- part12-Part12_06.Numbers/src/main/java/Program.java
+++ part12-Part12_06.Numbers/src/main/java/Program.java
@@ -6,6 +6,13 @@ public class Program {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("How many random numbers should be printed?");
+        int input = Integer.valueOf(scanner.nextLine());
+        Random random = new Random();
+        for (int i = 0; i < input; i++) {
+            System.out.println(random.nextInt(11));
+            
+        }
     }
 
 }
